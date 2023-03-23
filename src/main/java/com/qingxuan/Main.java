836bde6e66259f68a88b4059b31395a12f029096
small_wqx <3108759501@qq.com>
2023-03-23 20:49:34 +0800

    first commit method!

--- src/main/java/com/qingxuan/Main.java
+++ src/main/java/com/qingxuan/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public void method1(){
+        System.out.println("新方法1");
+    }
 }
